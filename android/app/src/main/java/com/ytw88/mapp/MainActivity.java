6350074c663df005c49b81534843fbe6235ac9cd
lingshiyao <471935003@qq.com>
2019-09-30 14:11:57 +0800

    更新模板工程

--- android/app/src/main/java/com/ytw88/mapp/MainActivity.java
+++ android/app/src/main/java/com/ytw88/mapp/MainActivity.java
@@ -2,20 +2,21 @@ package com.ytw88.mapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-
 import com.ytw.sdk.MPSDK;
-
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            MPSDK.launchApp(BuildConfig.appId, this);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        this.finish();
+        setContentView(R.layout.main);
+
+        findViewById(R.id.button).setOnClickListener(v -> {
+            try {
+                MPSDK.launchApp(BuildConfig.appId, MainActivity.this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
